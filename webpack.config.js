72dcf9f8e40e9846b4bf9d2087d1df1694ec8be4
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-10-14 11:02:25 +0100

    Use template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: { 
@@ -8,7 +9,14 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html'
+    }),
+  ],
   module: {
     rules: [
       {
